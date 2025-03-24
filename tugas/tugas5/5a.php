f19d92ec8aeb49dae823e374a4dd06999e9f6344
bilker29 <billywicaksono.999@gmail.com>
2025-03-25 00:12:46 +0700

    Memperbaiki dan Menyelesaikan Tugas 5

--- tugas/tugas5/5a.php
+++ tugas/tugas5/5a.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas 5a</title>
+    <style>
+        ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+        ul li img {
+            width: 100px;
+            height: auto;
+        }
+    </style>
 </head>
 <body>
     <?php
@@ -13,102 +24,102 @@
             "nrp" => "243040082",
             "email" => "MIFTAH.243040082@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Muhammad Nur Salam",
             "nrp" => "243040083",
             "email" => "NUR.243040083@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Nazala Qisti",
             "nrp" => "243040084",
             "email" => "NAZALA.243040084@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Wanita.jpg"
         ],
         [
             "nama" => "Muhammad Faisyal Nur Pratama",
             "nrp" => "243040085",
             "email" => "FAISYAL.243040085@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Ghryshvi Taushiyah Dzickra",
             "nrp" => "243040086",
             "email" => "GHRYSHVI.243040086@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Muhammd Farrel Triputra Alfarizqi",
             "nrp" => "243040087",
             "email" => "FARREL.243040087@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Rifqi Atha dwi Fahrezi",
             "nrp" => "243040088",
             "email" => "RIFQI.243040088@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Arneu Raysa",
             "nrp" => "243040089",
             "email" => "ARNEU.243040089@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Wanita.jpg"
         ],
         [
             "nama" => "Billy Wicaksono",
             "nrp" => "243040090",
             "email" => "BILLY.243040090@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Adriel Reihansyah Setiawan",
             "nrp" => "243040091",
             "email" => "ADRIEL.243040091@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Afsal Prima Maulana",
             "nrp" => "243040092",
             "email" => "AFSAL.243040092@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Akbar Putra Pamungkas",
             "nrp" => "243040093",
             "email" => "AKBAR.243040093@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],
         [
             "nama" => "Muhammad Fatur Rahman",
             "nrp" => "243040094",
             "email" => "FATUR.243040094@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Pria.jpg"
         ],[
             "nama" => "Almira Alfia Suparya Putri",
             "nrp" => "243040097",
             "email" => "ALMIRA.243040097@mail.unpas.ac.id",
             "jurusan" => "Teknik Informatika",
-            "gambar" => "nabila.jpg"
+            "gambar" => "Wanita.jpg"
         ],
     ];
     ?>
 
-    <h1>Daftar Mahasiswa</h1>
+    <h1>Daftar Mahasiswa Teknik Informatika C</h1>
 
     <?php foreach ($mahasiswa as $mhs) : ?>
         <ul>
@@ -121,17 +132,5 @@
             <li>JURUSAN : <?= $mhs["jurusan"]; ?></li>
         </ul>
     <?php endforeach; ?>
-
-    <h1>Daftar Mahasiswa TIF C</h1>
-
-    <?php foreach ($mahasiswa as $mhs) : ?>
-        <ul>
-            <li>Nama : <?= $mhs["nama"]; ?></li>
-            <li>NRP : <?= $mhs["nrp"]; ?></li>
-            <li>Jurusan : <?= $mhs["jurusan"]; ?></li>
-            <li>Email : <?= $mhs["email"]; ?></li>
-        </ul>
-    <?php endforeach; ?>
-
 </body>
 </html>
